4a251585ab879f14535dccfa044181e1416328cd
Moritz Jacobs <mail@moritzjacobs.de>
2024-01-11 09:52:49 +0100

    wip

--- apps/docs/app/routes/_index.tsx
+++ apps/docs/app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction, LinksFunction } from "@remix-run/node";
+
 import ReadMe from "../README.mdx";
+import stylesheet from "../tailwind.css";
 
 export const meta: MetaFunction = () => {
 	return [
@@ -10,6 +12,7 @@ export const meta: MetaFunction = () => {
 
 export const links: LinksFunction = () => {
 	return [
+		{ rel: "stylesheet", href: stylesheet },
 		{
 			rel: "icon",
 			href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧢</text></svg>",
@@ -18,7 +21,11 @@ export const links: LinksFunction = () => {
 };
 
 const Index = () => {
-	return <ReadMe />;
+	return (
+		<main className="flex gap-4 flex-col py-4 lg:py-12 max-w-screen-sm m-auto">
+			<ReadMe />
+		</main>
+	);
 };
 
 export default Index;
