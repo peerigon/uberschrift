2a9b61eabfd900ec4365aa46928efbda50d9f2e5
Moritz Jacobs <mail@moritzjacobs.de>
2024-04-23 11:05:57 +0200

    wip

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -1,28 +1,33 @@
-REPO=$(pwd)/packages/react
+CWD=$(pwd)
 
 # make a temporary directory
 TMP=$(mktemp -d 2>/dev/null || mktemp -d -t 'pprtmp')
 TYPE=$1
 
 # clean up
-# trap '{ rm -rf -- "$TMP"; }' EXIT
-trap '{ echo $TMP; }' EXIT
+trap '{ rm -rf -- "$TMP"; }' EXIT
+# trap '{ echo $TMP; }' EXIT
 
 # copy the boilerplate to the temporary directory
 cp -r ./integration-test/integration.test.ts.hbs ${TMP}/integration.test.ts
 
 if [ "$TYPE" = "local" ]; then
-	cd $REPO
+	cd $CWD/packages/react
+	npm install
+	npm run build
+
+	cd $CWD/packages/vue
 	npm install
 	npm run build
 
 	cd $TMP
 	npm init -y
-	npm install $REPO
+	npm install $CWD/packages/react
+	npm install $CWD/packages/vue
 else
 	cd ${TMP}
 	npm init -y
-	npm install uberschrift
+	npm install uberschrift vueberschrift
 fi
 
 # install dependencies
